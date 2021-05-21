e06bb83d4d65b433c7efd73d837fd2cb8927aca7
Mariusz `shd` Gliwiński <shd@nawia.net>
2021-05-21 15:48:33 +0200

    [tests] broken transaction perform implementation should not be there

--- tests/Api/PaymentsTransactionPerformTest.php
+++ tests/Api/PaymentsTransactionPerformTest.php
@@ -5,10 +5,6 @@ namespace EscolaLms\Payments\Tests\Api;
 class PaymentsTransactionPerformTest extends \EscolaLms\Payments\Tests\TestCase
 {
     public function testStudentCanPerformRegisteredPayment() {
-        $user = $this->makeStudent();
-        $registration = Transaction::factory()->makeOne();
-        $registration->buyer_id = $user->id;
-
         //@todo login as student
         //@todo register students payment internally
         //@todo perform a payment through the api
