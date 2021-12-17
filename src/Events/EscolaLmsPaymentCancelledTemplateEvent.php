3d480057aecd744e3a5a48069ed564a51663b914
Hubert Krzysztofiak <hubertkrzyszt@gmail.com>
2021-12-17 09:16:41 +0100

    Remove missing uses

--- src/Events/EscolaLmsPaymentCancelledTemplateEvent.php
+++ src/Events/EscolaLmsPaymentCancelledTemplateEvent.php
@@ -2,9 +2,6 @@
 
 namespace EscolaLms\Payments\Events;
 
-use EscolaLms\Payments\Models\Payment;
-use Illuminate\Contracts\Auth\Authenticatable;
-
 class EscolaLmsPaymentCancelledTemplateEvent extends EscolaLmsPaymentTemplateEvent
 {
 }
