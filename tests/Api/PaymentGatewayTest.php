168a9518d53f4b110deaf8dab4947f02a4622914
Paweł Chołaj <pawel.cholaj@escolasoft.com>
2022-03-23 11:02:32 +0100

    Test

---
+++ tests/Api/PaymentGatewayTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace EscolaLms\Payments\Tests\Api;
+
+use EscolaLms\Core\Tests\CreatesUsers;
+use EscolaLms\Payments\Facades\Payments;
+use EscolaLms\Payments\Gateway\Drivers\Przelewy24Driver;
+use EscolaLms\Payments\Gateway\Drivers\StripeDriver;
+use EscolaLms\Payments\Tests\Traits\CreatesBillable;
+
+class PaymentGatewayTest extends \EscolaLms\Payments\Tests\TestCase
+{
+    use CreatesBillable;
+
+    public function testStudentCanListGatewaysWithRequiredParams()
+    {
+        $billable = $this->createBillableStudent();
+
+        $response = $this->actingAs($billable)->json('GET', 'api/payments-gateways/', [
+            'per_page' => 20
+        ]);
+        $response->assertOk();
+        $response->assertJsonFragment([
+            'default_gateway' => Payments::getPaymentsConfig()->getDefaultGateway(),
+            'gateways' => [
+                'stripe' => [
+                    'enabled' => Payments::getPaymentsConfig()->isStripeEnabled(),
+                    'parameters' => StripeDriver::requiredParameters()
+                ],
+                'przelewy24' => [
+                    'enabled' => Payments::getPaymentsConfig()->isPrzelewy24Enabled(),
+                    'parameters' => Przelewy24Driver::requiredParameters()
+                ]
+            ]
+        ]);
+    }
+}
